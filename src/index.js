211642a5c9c7f91c3eb04765669b91809d7c1dc0
Charlie van Rantwijk <charlievanrantwijk@gmail.com>
2019-10-28 21:42:18 +0100

    add jsx function example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'Click on me!';
+}
+
 // create react component
 const App = function() {
-  return <div>Hi Loiss</div>;
+  return (
+    <div>
+      <label className="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
+    </div>
+  );
 };
 
 // take component and show it
